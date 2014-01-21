882246758cafb24ec05bb4b01cf8060c5767079d
Christian Briones <cwbriones@gmail.com>
2014-01-20 18:47:41 -0800

    init project

---
+++ diff.py
@@ -0,0 +1,23 @@
+"""
+Python implementation of diff.
+"""
+import sys
+
+class Diff(object):
+    """
+    Solves the longest common subsequence problem for two strings to find
+    their difference.
+    """
+
+    def __init__(self, str1, str2):
+        self.str1 = str1
+        self.str2 = str2
+
+def main(argv):
+    """
+    Entry point.
+    """
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
